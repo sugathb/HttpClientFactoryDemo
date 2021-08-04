c0276fc8ec577d81088cec84243b807849e7416b
sugathb <sugathb@gmail.com>
2021-08-04 23:55:19 +1000

    added-application-services

--- MusicBrainzDemo/MusicBrainzDemo.ApplicationServices/GetArtistsQueryHandler.cs
+++ MusicBrainzDemo/MusicBrainzDemo.ApplicationServices/GetArtistsQueryHandler.cs
@@ -1,6 +1,41 @@
-﻿namespace MusicBrainzDemo.ApplicationServices
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using MusicBrainzDemo.Infrastructure;
+
+namespace MusicBrainzDemo.ApplicationServices
 {
-    public class GetArtistsQueryHandler
+    public class GetArtistsQuery : IRequest<ArtistSearchResponse>
     {
+        public GetArtistsQuery(string artistName)
+        {
+            ArtistName = artistName;
+        }
+
+        public string ArtistName { get; }
+    }
+
+    public class GetArtistsQueryHandler : IRequestHandler<GetArtistsQuery, ArtistSearchResponse>
+    {
+        private const string InvalidArtistNameErrorMessage = "Invalid artist name.";
+
+        private readonly ISearchMusicClient _searchMusicClient;
+
+        public GetArtistsQueryHandler(ISearchMusicClient artistsSearchClient)
+        {
+            _searchMusicClient = artistsSearchClient;
+        }
+
+        public async Task<ArtistSearchResponse> Handle(GetArtistsQuery request, CancellationToken cancellationToken)
+        {
+            if (!Regex.Match(request.ArtistName, "^[A-Z][a-zA-Z]*$").Success)
+            {
+                throw new ArgumentException(InvalidArtistNameErrorMessage);
+            }
+
+            return await _searchMusicClient.FilterArtistsByNameAsync(request.ArtistName).ConfigureAwait(false);
+        }
     }
 }
---
+++ MusicBrainzDemo/MusicBrainzDemo.ApplicationServices/GetReleasesQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using MusicBrainzDemo.Infrastructure;
+
+namespace MusicBrainzDemo.ApplicationServices
+{
+    public class GetReleasesQuery : IRequest<ReleasesSearchResponse>
+    {
+        public GetReleasesQuery(Guid artistId)
+        {
+            ArtistId = artistId;
+        }
+
+        public Guid ArtistId { get; }
+    }
+
+    public class GetReleasesQueryHandler : IRequestHandler<GetReleasesQuery, ReleasesSearchResponse>
+    {
+        private readonly ISearchMusicClient _searchMusicClient;
+
+        public GetReleasesQueryHandler(ISearchMusicClient artistsSearchClient)
+        {
+            _searchMusicClient = artistsSearchClient;
+        }
+
+        public async Task<ReleasesSearchResponse> Handle(GetReleasesQuery request, CancellationToken cancellationToken)
+        {
+            return await _searchMusicClient.FilterReleasesByArtistIdAsync(request.ArtistId).ConfigureAwait(false);
+        }
+    }
+}
