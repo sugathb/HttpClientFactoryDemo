cbd884824161d394c9deb4e5a7cef5118b688d61
sugathb <sugathb@gmail.com>
2021-08-09 00:46:45 +1000

    code-cleanup

--- MusicBrainzDemo/MusicBrainzDemo.ApplicationServices/GetArtistsQueryHandler.cs
+++ MusicBrainzDemo/MusicBrainzDemo.ApplicationServices/GetArtistsQueryHandler.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Text.RegularExpressions;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using MusicBrainzDemo.Infrastructure;
@@ -19,22 +17,15 @@ namespace MusicBrainzDemo.ApplicationServices
 
     public class GetArtistsQueryHandler : IRequestHandler<GetArtistsQuery, ArtistSearchResponse>
     {
-        private const string InvalidArtistNameErrorMessage = "Invalid artist name.";
-
         private readonly ISearchMusicClient _searchMusicClient;
 
-        public GetArtistsQueryHandler(ISearchMusicClient artistsSearchClient)
+        public GetArtistsQueryHandler(ISearchMusicClient searchMusicClient)
         {
-            _searchMusicClient = artistsSearchClient;
+            _searchMusicClient = searchMusicClient;
         }
 
         public async Task<ArtistSearchResponse> Handle(GetArtistsQuery request, CancellationToken cancellationToken)
         {
-            if (!Regex.Match(request.ArtistName, "^[A-Z][a-zA-Z]*$").Success)
-            {
-                throw new ArgumentException(InvalidArtistNameErrorMessage);
-            }
-
             return await _searchMusicClient.FilterArtistsByNameAsync(request.ArtistName).ConfigureAwait(false);
         }
     }
