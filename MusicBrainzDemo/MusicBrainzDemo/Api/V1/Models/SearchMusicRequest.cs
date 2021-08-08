86142b3e41272b3f0a01d2ce5f8083ad70d4e1ec
sugathb <sugathb@gmail.com>
2021-08-09 00:38:00 +1000

    regex-error-fixed

--- MusicBrainzDemo/MusicBrainzDemo/Api/V1/Models/SearchMusicRequest.cs
+++ MusicBrainzDemo/MusicBrainzDemo/Api/V1/Models/SearchMusicRequest.cs
@@ -5,7 +5,7 @@ namespace MusicBrainzDemo.Api.V1.Models
     public class SearchMusicRequest
     {
         [Required(ErrorMessage = "Artist name cannot be are empty.")]
-        [RegularExpression(@"^[a-zA-Z\\s]*$", ErrorMessage = "Please enter a valid artist name.")]
+        [RegularExpression("^[a-zA-Z\\s]*$", ErrorMessage = "Please enter a valid artist name.")]
         public string ArtistName { get; set; }
     }
 }
